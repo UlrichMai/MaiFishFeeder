5f697683291ab961f23eb2d6957ab102bca35859
Ulrich Mai <ulrich.mai@outlook.com>
2020-10-19 08:31:56 +0200

    test servo positioning

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
 #include <Servo.h>
+
+#include "Private.h"
 
 Servo myservo;
 // cable colors SG90 servo:
@@ -10,19 +14,30 @@ int i;
 
 void setup() {
   Serial.begin(115200);
-  myservo.attach(5);
+
+	WiFi.persistent(false);
+	WiFi.mode(WIFI_STA);
+	WiFi.setAutoReconnect(true);
+	WiFi.begin(ssid, password);
+
+  ArduinoOTA.setHostname("FishFeeder");
+  //ArduinoOTA.setPassword(passwordOTA);
+  ArduinoOTA.begin();
+
+  myservo.attach(4);
   p=0;
   delay(5000);
 }
 
 void loop() {
-
+  ArduinoOTA.handle();  
+  
   if(p==0) p=15;
 
   i = map(p, 1, 14, 750, 2600); 
   Serial.printf("loch=%d, pos=%d\n",p,i);
   myservo.writeMicroseconds(i);   
-  delay(2000);
+  delay(10000);
   p--;
 
 }
